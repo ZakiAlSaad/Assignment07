a045096b6f52201e2c8f06750e132f7c8bb58123
ZakiAlSaad <onlyzakiofficial@gmail.com>
2026-04-17 15:31:47 +0600

    Reload Animation added

--- src/components/ui/Spinner.jsx
+++ src/components/ui/Spinner.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { CircleLoader } from "react-spinners";
 
 const Spinner = () => {
   return (
-    <div className=" flex flex-col justify-center items-center my-20">
-      <CircleLoader color="#244D3F" size={100} width={9} />
+    <div className="flex flex-col justify-center items-center my-20">
+      <div className="relative flex justify-center items-center w-24 h-24">
+        <div className="absolute w-full h-full border-4 border-[#244D3F] border-dashed rounded-full animate-[spin_3s_linear_infinite]" />
+        <div className="absolute w-16 h-16 border-4 border-[#244D3F]/30 border-t-[#244D3F] rounded-full animate-spin" />
+      </div>
     </div>
   );
 };
